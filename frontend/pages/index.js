11aeadd64aa4bf8059eb4f57f0d43b540a425b84
kelve Oliveira <102033649+kelve052@users.noreply.github.com>
2023-12-15 13:05:01 -0300

    add (page home) create page home

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,6 @@
 import styles from '@/styles/index.module.css'
 import Header from '@/components/Header/header'
+import Image from 'next/image'
 import CardPlayerSetings from '@/components/cardPlayerSettings/cardPlayerSttings'
 import Historicy from '@/components/historic/historicy'
 import InputBar from '@/components/inputBar/inputBar'
@@ -8,11 +9,16 @@ export default function Home() {
   return (
     <section className={styles.index}>
      <Header  />
-      <InputBar valueInvalid={false}  text={"Nome"} placeholder={"JoaninhaDoPneu"}/>
-      <InputBar valueInvalid={false}  text={"Nome"} widthImput={317} placeholder={"JoaninhaDoPneu"}/>
-      <InputBar valueInvalid={false}  text={"Nome"} widthImput={317} placeholder={"JoaninhaDoPneu"}/>
-      <InputBar valueInvalid={false}  text={"Nome"} widthImput={317} placeholder={"JoaninhaDoPneu"}/>
-      <InputBar valueInvalid={false}  text={"Nome"} widthImput={317} placeholder={"JoaninhaDoPneu"}/>
+     <h1 className={styles.h1}>Bem-vindos ao caixa do Banco Imobiliário</h1>
+     <p className={styles.p_defina_os}>Defina os participantes para iniciar</p>
+     <button className={styles.button_adicionar}>Adicionar participantes</button>
+
+     {/* backgrrounds */}
+     <Image className={styles.imgs_backgrounds_1} src={"/images/backgrounds/background-1.png"} width={108} height={258}></Image>
+     <Image className={styles.imgs_backgrounds_2} src={"/images/backgrounds/background-2.png"} width={126} height={126}></Image>
+     <Image className={styles.imgs_backgrounds_3} src={"/images/backgrounds/background-3.png"} width={90} height={67}></Image>
+     <Image className={styles.imgs_backgrounds_4} src={"/images/backgrounds/background-4.png"} width={80} height={55} ></Image>
+     
     </section>
   )
 }
