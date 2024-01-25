9aefd7533cdb408f89ee833bfbf56beb827d3842
kelve Oliveira <102033649+kelve052@users.noreply.github.com>
2024-01-25 17:24:56 -0300

    refactory: port api

--- api/src/sever.js
+++ api/src/sever.js
@@ -4,8 +4,9 @@ import { connectDB } from "./database/db.js";
 export const start = async () => {
   try {
     console.log('MONGO_URI:', process.env.MONGO_URI);
+    const port = process.env.PORT || 4000
     await connectDB(process.env.MONGO_URI);
-    const server = aplicacao.listen(4000, () => console.log('Server online on port 4000'));
+    const server = aplicacao.listen(port, () => console.log('Server online on port 4000'));
   } catch (error) {
     console.log('Error connecting:', error);
   }
