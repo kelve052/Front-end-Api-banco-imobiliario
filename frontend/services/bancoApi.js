53c6795a7457d7bc316ff014d23fad7c47eb009d
kelve Oliveira <102033649+kelve052@users.noreply.github.com>
2024-01-25 17:45:09 -0300

    refactory: update frontEnd/services alter port api

--- frontend/services/bancoApi.js
+++ frontend/services/bancoApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class ApiBanco{
   constructor(){
     this.server = axios.create({
-      baseURL:  process.env.NEXT_PUBLIC_API_URL
+      baseURL:  process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000"
     })
   }
   listarBancos = async ()=>{
