e64a74350a225a58820c0fd1f0a28b3695d01e46
kelve Oliveira <102033649+kelve052@users.noreply.github.com>
2024-01-26 16:04:01 -0300

    update vercel

--- api/src/sever.js
+++ api/src/sever.js
@@ -6,7 +6,7 @@ export const start = async () => {
     console.log('MONGO_URI:', process.env.MONGO_URI);
     const port = process.env.PORT || 4000
     await connectDB(process.env.MONGO_URI);
-    const server = aplicacao.listen(port, () => console.log('Server online on port 4000'));
+    const server = aplicacao.listen(port, () => console.log(`Server online on port ${port}`));
   } catch (error) {
     console.log('Error connecting:', error);
   }
